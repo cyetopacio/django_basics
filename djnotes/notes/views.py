a99738cb4ac6d57aef599406dd2de1e3d7800142
cyreen <cyemae@gmail.com>
2016-11-17 16:43:03 +0800

    Add urls

--- djnotes/notes/views.py
+++ djnotes/notes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import View
 
+
+class Index(View):
+    template_name = 'notes/index.html'
+    
+    def get_context_data(self):
+        context = {}
+        return context
+        
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data()
+        return render(request, self.template_name, context)
 # Create your views here.
