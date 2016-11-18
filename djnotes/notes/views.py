af74f30d2a03b9c2eef1b5b26e744da555d63c21
cyreen <cyemae@gmail.com>
2016-11-18 18:22:03 +0800

    Notes-Django

--- djnotes/notes/views.py
+++ djnotes/notes/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.http import Http404
 
 from .models import Note
+from .forms import NoteForm
+
 class Index(View):
     template_name = 'notes/index.html'
     
@@ -15,3 +18,37 @@ class Index(View):
         context = self.get_context_data()
         return render(request, self.template_name, context)
 # Create your views here.
+
+    def post(self, request, *args, **kwargs):
+        form = NoteForm(request.POST)
+        context = self.get_context_data()
+        if form.is_valid():
+            form.save()
+        return render(request, self.template_name, context)
+        
+        
+class Update(View):
+    template_name = 'notes/update.html'
+    
+    def get_context_data(self, **kwargs):
+        context = {}
+        try:
+            note_id = kwargs.get('pk', 0)
+            note = Note.objects.get(id=note_id)
+        except Note.DoesNotExist:
+            raise Http404
+        else:
+            context['note'] = note
+        return context
+        
+        
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return render(request, self.template_name, context)
+        
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        form = NoteForm(request.POST, instance=context.get('note'))
+        if form.is_valid():
+            form.save()
+        return render(request, self.template_name, context)
