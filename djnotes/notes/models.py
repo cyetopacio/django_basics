19c6b29d5c8aad35ca5838354baffe46ff4bbd2c
cyreen <cyemae@gmail.com>
2016-11-23 11:12:37 +0800

    Migrations

--- djnotes/notes/models.py
+++ djnotes/notes/models.py
@@ -6,7 +6,15 @@ from django.db import models
 class Note(models.Model):
     title = models.CharField(max_length=512, blank=True)
     note = models.TextField(blank=True)
+    labels = models.ManyToManyField('Label', blank=True)
     
     def __str__(self):
         return self.title
-      
+
+
+class Label(models.Model):
+    title = models.CharField(max_length=32, unique=True)
+
+
+    def __str__(self):
+        return self.title
