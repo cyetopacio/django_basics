9ecb2d30079382b781aad8d94adaf78dacf4b471
cyreen <cyemae@gmail.com>
2016-11-23 10:56:15 +0800

    Delete

--- djnotes/notes/views.py
+++ djnotes/notes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.http import Http404
 
@@ -52,3 +52,12 @@ class Update(View):
         if form.is_valid():
             form.save()
         return render(request, self.template_name, context)
+        
+class Delete( Update ):
+    template_name = 'notes/delete.html'
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        note = context.get('note')
+        note.delete()
+        return redirect('index')        
